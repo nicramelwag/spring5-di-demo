7f038e0849511e6065314a451d541552b07438a3
Marcin Gawle <marcingawle@onet.pl>
2018-10-01 10:48:32 +0200

    My version

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,5 +1,7 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -8,8 +10,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
+    @Autowired
+    private GreetingService greetingService;
+
     public String hello(){
-        System.out.println("Hello!!! ");
+        System.out.println(greetingService.sayGreeting());
 
         return "foo";
     }
---
+++ src/main/java/guru/springframework/services/PrimaryPolishGreetingService.java
@@ -0,0 +1,15 @@
+package guru.springframework.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("pl")
+@Primary
+public class PrimaryPolishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Czesc, pozdrawiam";
+    }
+}
